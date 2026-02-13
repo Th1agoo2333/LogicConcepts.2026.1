8da85026cb63cfc8ce43334592650a4348035c3d
USUARIO <USUARIO@DESKTOP-AS1HLFK>
2026-02-12 21:24:26 -0500

    DATE

---
+++ OPPconcepts/OPPconcepts.backend/Date.cs
@@ -0,0 +1,54 @@
+﻿namespace OPPconcepts.backend
+{
+    public class Date
+    {
+        private int _day;
+        private int _month;
+        private int _year;
+
+        public Date()
+        {
+            _year = 1900;
+            _month = 1;
+            _day = 1;
+        }
+
+        public Date(int year, int month, int day)
+        {
+            _day = day;
+            _year = year;
+            _month = month;
+        }
+
+        public int Year { 
+            get => _year; 
+            set => _year = ValidateYear(value); 
+        }  
+        public int Month { 
+            get => _month; 
+            set => _month = value; 
+        }
+        public int Day { 
+            get => _day;
+            set => _day = value;
+        }   
+
+        public override string ToString()
+        {
+            return $"{Day}/{Month}/{Year}";
+        }
+        
+        private int ValidateYear(int year)
+        {
+            if (year < 0 || year > 2100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year), "The year must be greater than 0");
+            }
+            return year;
+        }
+
+
+    }
+   
+
+}
---
+++ OPPconcepts/OPPconcepts.frontend/Program.cs
@@ -0,0 +1,8 @@
+﻿using OPPconcepts.backend;
+
+var date1 = new Date();
+var date2 = new Date(2026, 2, 12);
+
+
+Console.WriteLine(date1);
+Console.WriteLine(date2);
