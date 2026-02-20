ac92ea1da4210856f91638dc093a55c3b7e34664
USUARIO <USUARIO@DESKTOP-AS1HLFK>
2026-02-20 09:52:00 -0500

    add employee class

---
+++ OPPconcepts/OPPconcepts.backend/Employee.cs
@@ -0,0 +1,35 @@
+﻿
+namespace OPPconcepts.backend;
+
+public abstract class Employee
+{
+       //constructors
+    protected Employee(int  id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate)
+       {
+        
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        IsActive = isActive;
+        BornDate = bornDate;
+        HireDate = hireDate;
+        
+    }
+
+    //properties
+    public int Id { get; set;}
+    public string FirstName { get; set;} = null!;
+    public string LastName { get; set; } = null!;
+    public bool IsActive { get; set;}
+    public Date BornDate { get; set; } = null!;
+    public Date HireDate { get; set; } = null!;
+
+    //methods
+    public override string ToString()
+    {
+        return $"{Id}/t{FirstName}/n/t" +
+            $"Value to pay.......{GetValueToPay(),20:c2}";
+    }
+    
+    public abstract decimal GetValueToPay();
+}
--- OPPconcepts/OPPconcepts.frontend/Program.cs
+++ OPPconcepts/OPPconcepts.frontend/Program.cs
@@ -1,7 +1,6 @@
 ﻿using OPPconcepts.backend;
 
-var date1 = new Date();
-var date2 = new Date(2026, 2, 12);
+
 
 
 Console.WriteLine(date1);
